67156e3fa6adaf2a9ea4d2ed87b11be35997e80b
flamealchemist <prashant.evans@gmail.com>
2014-04-13 02:14:15 +0530

    Added Bank, Comment, Department, Notification Respository

---
+++ Apricot.Solution/Apricot.Data/Repositories/DepartmentRepositories.cs
@@ -0,0 +1,72 @@
+﻿using Apricot.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apricot.Data.Repositories
+{
+    class DepartmentRepositories
+    {
+        Department department;
+        ApricotContext _context;
+        /// <summary>
+        /// Constructor of DepartmentRepository
+        /// </summary>
+        /// <param name="context">Accepts Context of the Application</param>
+        public DepartmentRepositories(ApricotContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException();
+            }
+            _context = context;
+        }
+        /// <summary>
+        /// Returns the Departmemt Identified by Specified DepartmentID
+        /// </summary>
+        /// <param name="Dept_ID">Accept DepartmentID</param>
+        /// <returns>Department if found else null</returns>
+        public Department GetByDepartmentID(Int64 Dept_ID)
+        {
+            department = _context.Departments.Find(Dept_ID);
+            return department;
+        }
+        /// <summary>
+        /// Adds Department in Database
+        /// </summary>
+        /// <param name="_dept">Accepts Instance of Department</param>
+        public void AddDepartment(Department _dept)
+        {
+            if (_dept == null)
+            {
+                throw new ArgumentNullException();
+            }
+            _context.Departments.Add(_dept);
+            return;
+        }
+        /// <summary>
+        /// Updates existing Department
+        /// </summary>
+        /// <param name="_dept">Accept Instance of Department</param>
+        public void UpdateDepartment(Department _dept)
+        {
+            if (_dept == null)
+            {
+                throw new ArgumentNullException();
+            }
+            department = _dept;
+            _context.SaveChanges();
+            return;
+        }
+        /// <summary>
+        /// Returns Whole Department Table
+        /// </summary>
+        /// <returns></returns>
+        public List<Department> GetAll()
+        {
+            return _context.Departments.ToList<Department>();
+        }
+    }
+}
