24e0e33a71e1b1c166fb282e7b2e70ed147f3a47
flamealchemist <prashant.evans@gmail.com>
2014-04-15 17:03:11 +0530

    Added BussinessLogic

---
+++ Apricot/Apricot.BusinessLogic/DepartmentBL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apricot.Data.Models;
+using Apricot.Data;
+using Apricot.Data.Repositories;
+
+namespace Apricot.BusinessLogic
+{
+    public class DepartmentBL
+    {
+        ApricotContext _context = new ApricotContext();
+        public DepartmentBL(ApricotContext context)
+        {
+            _context = context;
+        }
+        public bool AddNewDepartment(string dept_name)
+        {
+            if(_context.Departments.Where(a => a.Dept_Name == dept_name).Single() != null )
+            {
+                return false;
+            }
+            Department dept = new Department()
+            {
+                Dept_Name = dept_name
+            };
+            DepartmentRepository _dept = new DepartmentRepository(_context);
+            _dept.AddDepartment(dept);
+            return true;
+        }
+        public bool EditDepartment( Department dept )
+        {
+            DepartmentRepository _dept = new DepartmentRepository(_context);
+            _dept.UpdateDepartment(dept);
+            return true;
+        }
+    }
+}
--- Apricot/Apricot.Data/Repositories/DepartmentRepository.cs
+++ Apricot/Apricot.Data/Repositories/DepartmentRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Apricot.Data.Repositories
 {
-    class DepartmentRepositories
+    public class DepartmentRepository
     {
         Department department;
         private readonly ApricotContext _context;
@@ -15,7 +15,7 @@ namespace Apricot.Data.Repositories
         /// Constructor of DepartmentRepository
         /// </summary>
         /// <param name="context">Accepts Context of the Application</param>
-        public DepartmentRepositories(ApricotContext context)
+        public DepartmentRepository(ApricotContext context)
         {
             if (context == null)
             {
@@ -44,6 +44,7 @@ namespace Apricot.Data.Repositories
                 throw new ArgumentNullException();
             }
             _context.Departments.Add(_dept);
+            _context.SaveChanges();
             return;
         }
         /// <summary>
@@ -67,6 +68,16 @@ namespace Apricot.Data.Repositories
         public IEnumerable<Department> GetAll()
         {
             return _context.Departments.ToList<Department>();
+        }
+        /// <summary>
+        /// Returns Department Identified By Specified Department Name
+        /// </summary>
+        /// <param name="_dept_name">Accepts string</param>
+        /// <returns>Department if found else null</returns>
+        public Department GetByDepartName(string _dept_name)
+        {
+            department = _context.Departments.Where(a => a.Dept_Name == _dept_name).Single();
+            return department;
         }
     }
 }
