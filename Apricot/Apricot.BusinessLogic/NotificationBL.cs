f3d8ce1550dd857db91274b3fbc51148523f0147
flamealchemist <prashant.evans@gmail.com>
2014-04-16 00:55:59 +0530

    Notification Business is under process

--- Apricot/Apricot.BusinessLogic/NotificationBL.cs
+++ Apricot/Apricot.BusinessLogic/NotificationBL.cs
@@ -39,9 +39,41 @@ namespace Apricot.BusinessLogic
         }
         public void NotificationOfRejection(Int64 bill_ID)
         {
-            Notification _ntf = new Notification();
+
             NotificationRepository ntf = new NotificationRepository(_context);
-            Bill bill = new B
+            Bill bill = new Bill();
+            bill = _context.Bills.Find(bill_ID);
+            Notification _ntf = new Notification()
+            {
+                Emp_ID = bill.Emp_ID,
+                Seen = false,
+                Ntf_Body = "Bill Number " + bill.Bill_ID.ToString() + " has been rejected",
+                Ntf_Subject = "Rejection of Bill"
+            };
+            ntf.AddNotification(_ntf);
+            return;
+        }
+        public void NotificationOfApproval(Int64 bill_ID)
+        {
+            NotificationRepository ntf = new NotificationRepository(_context);
+            Bill bill = new Bill();
+            bill = _context.Bills.Find(bill_ID);
+            Notification _ntf = new Notification()
+            {
+                Emp_ID = bill.Emp_ID,
+                Seen = false,
+                Ntf_Body = "Bill Number " + bill.Bill_ID.ToString() + " has been approved",
+                Ntf_Subject = "Approval of Bill"
+            };
+            ntf.AddNotification(_ntf);
+            Bill_FM bill_fm = new Bill_FM();
+            BillFMRepository bfm = new BillFMRepository(_context);
+            bill_fm = bfm.GetByBillId(bill_ID);
+            _ntf.Emp_ID = bill_fm.Bill_FM_ID;
+            _ntf.Ntf_Body = "New bill for financing has arrived";
+            _ntf.Ntf_Subject = "New Bill";
+            ntf.AddNotification(_ntf);
+            return;
         }
 
     }
