be8b0ecf6122244f2415b67d28e6bc4456321ca1
kumar-dev <dev24d@gmail.com>
2014-04-16 00:20:28 -0700

    Added empty controller ManageEmployeeController
    Added FillFullEmployee class

---
+++ Apricot/Apricot.Web/App_Code/FillFullEmployee.cs
@@ -0,0 +1,25 @@
+﻿using Apricot.Data;
+using Apricot.Data.Models;
+using Apricot.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Apricot.Data.Repositories;
+
+namespace Apricot.Web.App_Code
+{
+    public class FillFullEmployee
+    {
+        private ApricotContext _context;
+        public FillFullEmployee(Data.ApricotContext _context) 
+        {
+            this._context = _context;
+        }
+
+        /*public IEnumerable<FullEmployee> GetAllFullEmployee(String EmployeeNumber)
+        {
+            List<FullEmployee> fullemployee = new List<FullEmployee>(0);
+        }*/
+    }
+}
---
+++ Apricot/Apricot.Web/Controllers/ManageEmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Apricot.Data.Models;
+using Apricot.Data.Repositories;
+using Apricot.Data;
+using Apricot.Web.Models;
+using Apricot.Web.App_Code;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Apricot.Web.Controllers
+{
+    public class ManageEmployeeController : Controller
+    {
+        //
+        // GET: /ManageEmployee/
+        public ActionResult Index()
+        {
+            var Db = new ApricotContext();
+            var full_employee = new FullEmployee();
+            return View();
+        }
+	}
+}
